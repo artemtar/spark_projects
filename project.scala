63763ceb1c6bf414ed619eedbdcdf1630542e246
artemtar <artemtarasov89@gmail.com>
2018-11-09 05:21:54 -0500

    update

--- project.scala
+++ project.scala
@@ -1,16 +1,35 @@
 
 object Cells {
+  //spark staff
   import org.apache.spark.sql.SparkSession
   import org.apache.spark.sql.DataFrame
   import org.apache.spark.sql.functions._
+  
   // io
   import scala.io._
   import com.fasterxml.jackson.databind.ObjectMapper
   import com.fasterxml.jackson.module.scala.DefaultScalaModule
   import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
-  // end io
+  import org.apache.spark.sql.SaveMode
   
-  class Data(val path: String) {
+  // ml
+  import org.apache.spark.ml.feature.{HashingTF, Tokenizer, StopWordsRemover, Word2Vec}
+  import scala.collection.mutable.WrappedArray
+  import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+  import org.apache.spark.mllib.regression.LabeledPoint
+  
+  //rest
+  import org.apache.spark.mllib.linalg.{Vector, Vectors, SparseVector}
+  
+  object MyConfig {
+    val NUM_OF_AUTHORS: Integer = 10
+    val TRAINING_PORTION = 0.9
+    val VALIDATING_PORTION = 0.1
+    var MINIUM_NUMBER_OF_POSTS: Integer = 0
+    val NumClasses = 2
+  }
+  
+  case class Data(val path: String) {
     
     def load(session: SparkSession, header: Boolean, schema: Boolean, separator: String): DataFrame = 
       session.read.option("header", "true")
@@ -30,6 +49,116 @@ object Cells {
       session.read.json(path)
     }
   }
+  
+  case class Reviewers(val unsorted: DataFrame){
+    
+    def top(selctOption: Seq[String], limit: Integer): DataFrame = {
+      val args = listToCol(selctOption)
+      val filtered = unsorted.select(args:_*)
+      val topCount = makeTop(filtered, limit, "reviewerID", "reviewerID")
+      filtered.join(topCount, col("reviewerID") === col("id"))
+    }
+    
+    def listToCol(selctOption: Seq[String]): Seq[org.apache.spark.sql.Column] = {
+      selctOption.map(name => col(name))
+    }
+    
+    def makeTop(filtered: DataFrame, lim: Integer, group: String, Id: String) = {
+    filtered.groupBy(group)
+          .count()
+          .sort(desc("count"))
+          .limit(lim)
+          .select(col(Id).alias("id"))
+    }
+  }
+  
+  case class DataPrep(val df: DataFrame){
+    
+    def MakeTokenizer(input: String) = {
+      new Tokenizer()
+    .setInputCol(input)
+    .setOutputCol("raw")
+    }
+    def MakeStopWordRemover(input: String) = {
+     new StopWordsRemover()
+    .setInputCol(input)
+    .setOutputCol("filtered")
+    }
+    def getW2V(input: String) = {
+        new Word2Vec()
+        .setInputCol(input)
+        .setOutputCol("result")
+        .setVectorSize(300)
+        .setMinCount(0)
+    }
+    
+    def tokenize(input: String): DataFrame = {
+      val tokenizer = MakeTokenizer(input)
+      tokenizer.transform(df)
+    }
+  
+    val cleaning: WrappedArray[String] => Array[String] = (v: WrappedArray[String]) => {
+      
+      val unwraped = v.asInstanceOf[WrappedArray[String]].toArray
+      val removeSpaseLowerCase =
+        for(e <- unwraped if !e.isEmpty())
+          yield e.trim.toLowerCase()
+      .replaceAll( """<(?!\/?a(?=>|\s.*>))\/?.*?>""", "")
+      removeSpaseLowerCase
+  //     .replaceAll("\\<[^>]*>", "")
+  
+  
+  }
+    val cleaningUDF = 
+      udf[Array[String], WrappedArray[String]](cleaning)
+    
+    def cleanData(dfDirty: DataFrame): DataFrame = {
+  //     val s1 = dfDirty.filter(col("raw").like("_"))
+                              
+  //       x != ':' || x != ';' || x != '/'  || x != '-' || x != '_' || x != ')' || x != '(' || x != '>' || x != '<')
+      
+      val s2 = dfDirty.withColumn("rawTrimed", cleaningUDF(col("raw")))
+      val remover = MakeStopWordRemover("rawTrimed")
+      val noStopWords = remover.transform(s2)
+      val vector = getW2V("filtered")
+      val model = vector.fit(noStopWords)
+      model.transform(noStopWords)
+    }
+  }
+  
+  case class AuthorModel(val AuthourId: String,
+                         val df: DataFrame,
+                         val training_size: Double,
+                         val validating_size: Double){
+    
+    val LogReg = new LogisticRegressionWithLBFGS()
+                                        .setNumClasses(MyConfig.NumClasses)
+  
+    def makeTestSet() = {
+      val rdd = df.rdd
+      val regData = rdd.map(row => { val lable = if (row.getAs[String]("reviewerID") == AuthourId) 1 else 0
+                          val vector = row.getAs[org.apache.spark.ml.linalg.SparseVector]("result").toDense
+                          LabeledPoint(lable, org.apache.spark.mllib.linalg.Vectors.fromML(vector))})
+      regData.randomSplit(Array(training_size.toDouble, validating_size.toDouble), 11L)
+    }
+    
+    def train(data: Array[org.apache.spark.rdd.RDD[org.apache.spark.mllib.regression.LabeledPoint]]) = {
+      
+      LogReg.optimizer.setRegParam(0.0)
+      val train = data(0).cache()
+      val validate = data(1).cache()
+      val model = LogReg.run(train)
+      model
+      
+    }
+    
+  }
+  
+  def mdgToVector(msg: String) = {
+    
+  }
+  
+  
   object SparkSessionCreate { 
     val createSession: SparkSession = { 
         SparkSession
@@ -39,27 +168,103 @@ object Cells {
         .getOrCreate()
     }
   }
+  
+  def writeTofile(df: DataFrame, path: String){
+    df.coalesce(1)
+    .write
+    .mode(SaveMode.Overwrite)
+    .json(path)
+  }
 
   /* ... new cell ... */
 
   val session: SparkSession = SparkSessionCreate.createSession
   val path: String = "/home/atarasov/Documents/111/test.json"
-  val data = new Data(path)
+  val data = Data(path)    
 
   /* ... new cell ... */
 
-  import session.implicits._
+  val selectList = Seq("reviewerID", "reviewerName", "reviewText", "summary")
+
+  /* ... new cell ... */
+
   val df = data.jsonToDF(session)
-  val df_filtered = df.select("reviewerID", "reviewerName", "reviewText", "summary")
-  val reviewers = df_filtered.groupBy("reviewerID").count()
-  val top_reviewers = reviewers.sort(desc("count")).take(20)
+  val topReviewrMaker = new Reviewers(df)
+  val dfTopReviers = topReviewrMaker.top(selectList, 10)
 
   /* ... new cell ... */
 
-  val top_reviewers_list = top_reviewers.map(x => x(0))
+  val cleaner = DataPrep(dfTopReviers)
+  val tok = cleaner.tokenize("reviewText")
+  val a = cleaner.cleanData(tok)
+  val b = a.select("raw", "rawTrimed", "filtered", "result")
+  val m = AuthorModel("AV6QDP8Q0ONK4", a, 0.9, 0.1)
+  val mm = m.makeTestSet()
+  val model = m.train(mm)
+  
+  // writeTofile(b, "/home/atarasov/Documents/111/out.json")
 
   /* ... new cell ... */
 
-  val contains: String => Boolean =
+  
+
+  /* ... new cell ... */
+
+  val nr = session.read.json(path)
+  val nf = nr.select("reviewerID", "reviewerName", "reviewText", "summary")
+  val reviewers = nr.groupBy("reviewerID").count()
+  reviewers.count
+
+  /* ... new cell ... */
+
+  val top_reviewers = reviewers.sort(desc("count")).limit(10)
+  val ress = top_reviewers.select($"count", $"reviewerID".alias("id"))
+  val post_num = ress.select("count").collect()(NUM_OF_AUTHORS - 1)
+  MINIUM_NUMBER_OF_POSTS = post_num(0).asInstanceOf[Long].toInt
+  
+  ress.show
+
+  /* ... new cell ... */
+
+  Vector(1)
+
+  /* ... new cell ... */
+
+  val r = ress.rdd
+  val s = "AV6QDP8Q0ONK4"
+  val rr = r.map(row => { val lable = if (row.getAs[String]("id") == s) 1 else 0
+                          val vector = Vectors(row.getAs[Float]("count"))
+                          LabeledPoint(lable, vector)})
+  rr.take(10)
+
+  /* ... new cell ... */
+
+  val another = nr.join(ress, $"reviewerID" === $"id")
+  val tt = new Tokenizer()
+    .setInputCol("reviewText")
+    .setOutputCol("raw")
+
+  /* ... new cell ... */
+
+  import scala.collection.mutable.WrappedArray
+  val yetanother = tt.transform(another).select("reviewText", "raw")
+  val removeWhiteSpaceAndLowerCase: WrappedArray[String] => Array[String] = (v: WrappedArray[String]) => {
+    val n: Array[String] = Array[String]()
+    val unwraped = v.asInstanceOf[WrappedArray[String]].toArray
+    for(e <- unwraped){n + e.toLowerCase().trim}
+    n}
+    
+    val removeWhiteSpaceAndLowerCaseUDF = udf[Array[String], WrappedArray[String]](removeWhiteSpaceAndLowerCase)
+  // yetanother.rdd.first
+  yetanother.withColumn("rawTrimed", removeWhiteSpaceAndLowerCaseUDF(yetanother("raw")))
+  // yetanother.collect()(1)
+
+  /* ... new cell ... */
+
+  
+
+  /* ... new cell ... */
+
+  session.stop
 }
                   
