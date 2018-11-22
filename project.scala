5be2334d3e103f5c6cce447c16d7c4ace4e6dd2b
artemtar <artemtarasov89@gmail.com>
2018-11-22 17:58:30 -0500

    added load/save functions

--- project.scala
+++ project.scala
@@ -5,7 +5,6 @@ object Cells {
   import org.apache.spark.sql.DataFrame
   import org.apache.spark.sql.functions._
   
-  
   // io
   import scala.io._
   import com.fasterxml.jackson.databind.ObjectMapper
@@ -15,18 +14,19 @@ object Cells {
   
   // ml
   import org.apache.spark.ml.feature.{HashingTF, Tokenizer, StopWordsRemover, Word2Vec}
-  import scala.collection.mutable.WrappedArray
   import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
   import org.apache.spark.mllib.regression.LabeledPoint
   
   //rest
   import org.apache.spark.mllib.linalg.{Vector, Vectors, SparseVector}
+  import scala.collection.mutable.WrappedArray
   
   object MyConfig {
     val NUM_OF_AUTHORS: Integer = 10
     val TRAINING_PORTION = 0.9
     val VALIDATING_PORTION = 0.1
     var MINIUM_NUMBER_OF_POSTS: Integer = 0
+    val VECTOR_SIZE = 100
     val NumClasses = 2
   }
   
@@ -89,7 +89,7 @@ object Cells {
         new Word2Vec()
         .setInputCol(input)
         .setOutputCol("result")
-        .setVectorSize(300)
+        .setVectorSize(MyConfig.VECTOR_SIZE)
         .setMinCount(0)
     }  
     
@@ -99,15 +99,28 @@ object Cells {
     } 
     
     val cleaning: WrappedArray[String] => Array[String] = (v: WrappedArray[String]) => {
-      
-      val unwraped = v.asInstanceOf[WrappedArray[String]].toArray
-      val removeSpaseLowerCase =
-        for(e <- unwraped if !e.isEmpty())
-          yield e.trim.toLowerCase()
-      .replaceAll( """<(?!\/?a(?=>|\s.*>))\/?.*?>""", "")
-      removeSpaseLowerCase
-  //     .replaceAll("\\<[^>]*>", "")
+      v.toArray
+  //     val unwraped = v.asInstanceOf[WrappedArray[String]].toArray
+  //     val removeSpaseLowerCase: Array[String] =
+  //       for(e <- unwraped if !e.isEmpty())
+  //         yield e.trim.toLowerCase()
+  //     .replaceAll( """<\(?!\/?a(?=>|\s.*>\)\\)\/?.*?>""", "")
+  //   removeSpaseLowerCase
+  //   val result = Array[String]()
+    
+  //   for(word <- removeSpaseLowerCase){
+  //     if (word.contains(".") && (word.count(_ == '.') == 1) && !(word.takeRight(1) == "."))
+  //         {
+  //           println(word)
+  //           var splited = word.split('.')
+  //           result = result :+ (splited(0) + ".")
+  //           result = result :+ splited(1)
+  //         }  
+  //     else result = result :+ word
+  //   }
+  //   result
   }
+    
     val cleaningUDF = 
       udf[Array[String], WrappedArray[String]](cleaning)
     
@@ -116,9 +129,9 @@ object Cells {
                               
   //       x != ':' || x != ';' || x != '/'  || x != '-' || x != '_' || x != ')' || x != '(' || x != '>' || x != '<')
       
-      val s2 = dfDirty.withColumn("rawTrimed", cleaningUDF(col("raw")))
-      val remover = MakeStopWordRemover("rawTrimed")
-      val noStopWords = remover.transform(s2)
+  //     val s2 = dfDirty.withColumn("rawTrimed", cleaningUDF(col("raw")))
+      val remover = MakeStopWordRemover("raw")
+      val noStopWords = remover.transform(dfDirty)
       val vector = getW2V("filtered")
       val model = vector.fit(noStopWords)
       model.transform(noStopWords)
@@ -172,20 +185,34 @@ object Cells {
         new Word2Vec()
         .setInputCol("filtered")
         .setOutputCol("result")
-        .setVectorSize(300)
+        .setVectorSize(MyConfig.VECTOR_SIZE)
         .setMinCount(0)  
     
     
-      val cleaning: WrappedArray[String] => Array[String] = (v: WrappedArray[String]) => {
+    val cleaning: WrappedArray[String] => Array[String] = (v: WrappedArray[String]) => {
       
       val unwraped = v.asInstanceOf[WrappedArray[String]].toArray
-      val removeSpaseLowerCase =
+      val removeSpaseLowerCase: Array[String] =
         for(e <- unwraped if !e.isEmpty())
           yield e.trim.toLowerCase()
-      .replaceAll( """<(?!\/?a(?=>|\s.*>))\/?.*?>""", "")
+      .replaceAll( """<\(?!\/?a(?=>|\s.*>\)\\)\/?.*?>""", "")
+    
+  //   var result = Array[String]()
+    
+  //   for(word <- removeSpaseLowerCase){
+  //     if (word.contains(".") && (word.count(_ == '.') == 1) && !(word.takeRight(1) == "."))
+  //         {
+  //           println(word)
+  //           var splited = word.split('.')
+  //           result = result :+ (splited(0) + ".")
+  //           result = result :+ splited(1)
+  //         }  
+  //     else result = result :+ word
+  //   }
+  //   result
       removeSpaseLowerCase
-  //     .replaceAll("\\<[^>]*>", "")
   }
+    
     val cleaningUDF = 
       udf[Array[String], WrappedArray[String]](cleaning)
     
@@ -217,8 +244,13 @@ object Cells {
     msgToVec(msg, session)
   }
   }
+  import org.apache.spark.sql.{DataFrame, SQLContext}
+  import org.apache.spark.{SparkConf, SparkContext}
   
   object SparkSessionCreate { 
+  //     val conf = new SparkConf().setAppName("project")
+  //     val sc: SparkContext = new SparkContext(conf)
+  //     val createSession: SQLContext = new SQLContext(sc)
     val createSession: SparkSession = { 
         SparkSession
         .builder()
@@ -237,80 +269,215 @@ object Cells {
 
   /* ... new cell ... */
 
-  case class Classifier(authorList: Array[(Any, LogisticRegressionWithLBFGS)]){
-    def classify() = {}
+  import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+  import org.apache.spark.sql.SparkSession
+  import org.apache.spark.sql.DataFrame
+  import org.apache.spark.sql.functions._
+  import org.apache.spark.mllib.classification.LogisticRegressionModel
+  // import org.apache.spark.ml.classification.LogisticRegression
+  
+  
+  case class Classifier(authorModelList: Array[(Any, LogisticRegressionModel)]){
+    def classify(msg: org.apache.spark.rdd.RDD[org.apache.spark.mllib.linalg.Vector]) = {
+      
+      val predictions = authorModelList.map(e => 
+                          {val model = e._2
+                          model.clearThreshold()
+                          val prediction = model.predict(msg)
+                           (e._1, prediction.collect)})
+      predictions.sortWith(_._2(0) > _._2(0))
+    }
+    
+    def getPrediction(msg: org.apache.spark.rdd.RDD[org.apache.spark.mllib.linalg.Vector]) = {
+      val arrayPrediction = classify(msg)
+      arrayPrediction(0)._1
+    }
     def save() = {}
   
   }
-  case class Controller(val df: DataFrame){
+  
+  object Controller{  
     
-    def createTopReviewrs(selectList: Seq): DataFrame = {
-      val topReviewrMaker = new Reviewers(df)
-      topReviewrMaker.top(selectList, 10)
-    }
+    def topReviewrsToList(topRev: DataFrame) = {
+      val selectList = "reviewerID"
+      val dfTopReviers = topRev.select(selectList)
+      (dfTopReviers.collect).toSet
+    }  
     
-    def topReviewrsToList() = {
-      val selectList = Seq("reviewerID")
-      val dfTopReviers = createTopReviewrs(selectList)
-      dfTopReviers.collect
-    }
-    
-    def cleanEachTopAuthor() = {
-      val selectList = Seq("reviewerID", "reviewerName", "reviewText", "summary")
-      val dfTopReviers = createTopReviewrs(selectList)
+    def cleanEachTopAuthor(dfTopReviers: DataFrame) = {
       val cleaner = DataPrep(dfTopReviers)
       val tokenized = cleaner.tokenize("reviewText")
       cleaner.cleanData(tokenized)    
     }
     
-    def makeAuthorModel(authourId: String, cleanData: DataFrame) = {
-      val select = a.select("raw", "rawTrimed", "filtered", "result")
-      val author = AuthorModel(authourId, DataFrame, 0.9, 0.1)
-      val authorTestTrainSet = m.makeTestSet()
-      m.train(authorTestTrainSet)
+    def makeAuthorModel(authourId: Any, cleanData: DataFrame) = {
+      val author = AuthorModel(authourId.asInstanceOf[String], cleanData, 0.6, 0.4)
+      val authorTestTrainSet = author.makeTestSet()
+      author.train(authorTestTrainSet)
     }
     
-    def getTrainedModelList() = {  
-      val authorList = topReviewrsToList()
-      val authorModels
-      authorList.map(e => (e(0), makeAuthorModel(e(0)))
-      Classifier(authorList)
+    def getTrainedModelList(trainSet: DataFrame, dfTopReviers: DataFrame) = {  
+      val authorList = (topReviewrsToList(dfTopReviers)).toArray
+      val authorModels = authorList
+                    .map(e => (e(0), makeAuthorModel(e(0), trainSet)))                       
+  //     Classifier(authorModels)
+      authorModels
     }
+    def getSession() = {SparkSessionCreate.createSession}
+    
+    def main() = {
+        val session: SparkSession = getSession()
+        val path: String = "/home/atarasov/Documents/111/test.json"
+        val data = Data(path)
+        val selectList = Seq("reviewerID", "reviewerName", "reviewText", "summary")
+      
+        val df = data.jsonToDF(session)
+        val topReviewrMaker = new Reviewers(df)
+        val dfTopReviers = topReviewrMaker.top(selectList, 2)
+      
+        val mainDataSet = cleanEachTopAuthor(df)
+        val arrayModel = getTrainedModelList(mainDataSet, dfTopReviers)
+        Classifier(arrayModel)
+    }
+    def load() = {}
   }
 
   /* ... new cell ... */
 
-  val session: SparkSession = SparkSessionCreate.createSession
+  import scala.collection.mutable.WrappedArray
+  val inp = Array[String]("new","years","eve,","four","people","bump","top","popular","suicide","building","intent","jumping.martin","sharp","(pierce","brosnan)","popular",
+                          "morning","show","host","family.","fell","grace","&#34;she","looked","like","25.&#34;maureen",
+                          "(toni","collette)","rather","mousey","person","never","lived","life.","cares","invalid","son","deeply","loves.jess","(imogen","poots)","daughter",
+                          "rich","politician.","sister","jennifer,","read","book","invisibility,","ironically","went","missing","two","years","ago.","currently","mad",
+                          "boyfriend","dumped","her.jj","(aaron","paul)","american","part","amateur","band.","claims","ccr","brain","cancer.the","press","found",
+                          "suicide","attempts","gain","15","minutes","fame.","turn","form","bond","become","support","group.this","another","magnolia","&#34;feel",
+                          "good&#34;","film.","seen","one","before,","know","feeling.","typically,","film","light","quirky","comedy,","messages","life","take","you,",
+                          "decent","acting","directing.","rosamund","pike","role","smaller","billing.parental","guide:","f-bomb.","sex.","brief","rear","nudity","(poots)")
+  val cleaning: WrappedArray[String] => Array[String] = (v: WrappedArray[String]) => {
+      
+      val unwraped = v.asInstanceOf[WrappedArray[String]].toArray
+      val removeSpaseLowerCase: Array[String] =
+        for(e <- unwraped if !e.isEmpty())
+          yield e.trim.toLowerCase()
+      .replaceAll( """<\(?!\/?a(?=>|\s.*>\)\\)\/?.*?>""", "")
+    
+    var result = Array[String]()
+    
+    for(word <- removeSpaseLowerCase){
+      if (word.contains(".") && (word.count(_ == '.') == 1) && !(word.takeRight(1) == "."))
+          {
+            println(word)
+            var splited = word.split('.')
+            result = result :+ (splited(0) + ".")
+            result = result :+ splited(1)
+          }  
+      else result = result :+ word
+    }
+    result
+  }
+
+  /* ... new cell ... */
+
+  val session = Controller.getSession()
   val path: String = "/home/atarasov/Documents/111/test.json"
-  val data = Data(path)    
-
-  /* ... new cell ... */
-
   val selectList = Seq("reviewerID", "reviewerName", "reviewText", "summary")
-
-  /* ... new cell ... */
-
+  val data = Data(path)    
   val df = data.jsonToDF(session)
   val topReviewrMaker = new Reviewers(df)
-  val dfTopReviers = topReviewrMaker.top(selectList, 10)
+  val dfTopReviers = topReviewrMaker.top(selectList, 1)
 
   /* ... new cell ... */
 
-  val cleaner = DataPrep(dfTopReviers)
+  val cleaner = DataPrep(df)
   val tok = cleaner.tokenize("reviewText")
-  val a = cleaner.cleanData(tok)
-  val whatIwhantToselect = a.select("raw", "rawTrimed", "filtered", "result")
+  val cl = cleaner.cleanData(tok)
+  // val whatIwhantToselect = a.select("raw", "filtered", "result")
+  cl.count
+  
+  // writeTofile(whatIwhantToselect.select("filtered", "raw"), "/home/atarasov/Documents/111/out.json")
+
+  /* ... new cell ... */
+
   val m = AuthorModel("AV6QDP8Q0ONK4", a, 0.9, 0.1)
   val mm = m.makeTestSet()
   val model = m.train(mm)
-  
-  // writeTofile(b, "/home/atarasov/Documents/111/out.json")
 
   /* ... new cell ... */
 
+  val session = Controller.getSession
   import session.implicits._
   val mm = MsgMaker()
-  val i = mm.msgToVec("should this match", session)
+  val i = mm.msgToVec("This show always is excellent, as far as british crime or mystery" +
+  "showsgoes this is one of the best ever made." +
+  "The stories are well done and the acting is top notch with interesting twists in the" +
+  "realistic and brutal storylines." +
+  "One episode is not on this disc the excellent 'prayer of the bone\" which is on a seperate disc.", session)
+
+  /* ... new cell ... */
+
+  val clas = Controller.main
+
+  /* ... new cell ... */
+
+  clas.classify(i)
+
+  /* ... new cell ... */
+
+  import java.io._
+  
+  def save(models: Array[(Any, org.apache.spark.mllib.classification.LogisticRegressionModel)]) = {
+    val path: String = "/home/atarasov/Documents/111/models"
+    val writer = new PrintWriter(path + "/models.txt")
+         models.map(e => 
+                          {
+                            val model = e._2
+                            val modelName = e._1
+                            writer.append(modelName.asInstanceOf[String] + ".")
+                            model.save(Controller.getSession.sparkContext, path + "/" + modelName)
+                          })
+    writer.close
+  }
+
+  /* ... new cell ... */
+
+  import scala.io.Source._
+  
+  def load(): Classifier = {
+    val path: String = "/home/atarasov/Documents/111/models"
+    val models = fromFile(path + "/models.txt").mkString
+    val modelsList = models.split('.')
+    val authorModelsList = modelsList.map(e => {
+                                          val model = org.apache.spark.mllib.classification.LogisticRegressionModel
+                                          .load(Controller.getSession.sparkContext, path + "/" + e)
+                                            (e.asInstanceOf[Any], model)})
+    Classifier(authorModelsList)
+  }
+
+  /* ... new cell ... */
+
+  val m = load()
+
+  /* ... new cell ... */
+
+  m.classify(i)
+
+  /* ... new cell ... */
+
+  save(clas.authorModelList)
+
+  /* ... new cell ... */
+
+  i.collect
+
+  /* ... new cell ... */
+
+  clas.classify(i)
+
+  /* ... new cell ... */
+
+  // val c = v.authorList(0)._2
+  model.clearThreshold()
+  model.predict(i).collect
 
   /* ... new cell ... */
 
